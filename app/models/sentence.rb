b7e965dc46b73d6e660f0c777dfc3636e989b86f
Austin Kincaid & Alexa Zeazas <student@epicodus.com>
2015-10-15 16:25:48 -0700

    Add peer review comments and changes

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -2,7 +2,7 @@ class Sentence < ActiveRecord::Base
   validates :author, :text, presence: true
   belongs_to :story
   validates :text, length: {minimum: 30, maximum: 200}
-  before_validation (:author_title_case)
+  before_save (:author_title_case)
 
 private
   def author_title_case
