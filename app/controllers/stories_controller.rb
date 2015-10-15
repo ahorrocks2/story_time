aed69267510d60b0f39887ed775cac1faac1a208
alyssa horrocks & julianna green <student@epicodus.com>
2015-10-15 10:28:54 -0700

    add sorting alphabetically by title on main page

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,6 @@
 class StoriesController < ApplicationController
   def index
-    @stories = Story.all
+    @stories = Story.all.order(title: :asc)
   end
 
   def show
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -2,4 +2,15 @@ class Sentence < ActiveRecord::Base
   validates :author, :text, presence: true
   belongs_to :story
   validates :text, length: {minimum: 30, maximum: 200}
+  before_validation (:author_title_case)
+
+private
+  def author_title_case
+    author = self.author.downcase
+    words = author.split(' ')
+    words.map do |word|
+      word.capitalize!()
+    end
+    self.author = words.join(' ')
+  end
 end
